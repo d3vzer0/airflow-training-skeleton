ddb917c398652328d0662657fc6e0534d676fc17
Joey Dreijer <jdreijer@schubergphilis.com>
2020-01-31 15:56:27 +0100

    ex5

---
+++ dags/ex5.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+import datetime
+from datetime import timedelta
+import airflow
+from airflow.models import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import BranchPythonOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.contrib.operators.postgres_to_gcs_operator import PostgresToGoogleCloudStorageOperator
+
+# from airflow.operators.postgres_to_gcs_operator import 
+# from airflow.providers.google.cloud.operators.postgres_to_gcs_operator import Postg
+
+
+args = {
+    'owner': 'JDreijer',
+    'start_date': airflow.utils.dates.days_ago(2),
+}
+
+dag = DAG(
+    dag_id='exercise5',
+    default_args=args,
+    schedule_interval= None,
+    dagrun_timeout=timedelta(minutes=60),
+)
+
+t1 = PostgresToGoogleCloudStorageOperator(
+    postgres_conn_id='postgres_ex',
+    sql='select * from land_registry_price_paid_uk LIMIT 5',
+    bucket='test_bucket_airflow',
+    filename='postgres_dump.csv',
+    google_cloud_storage_conn_id='google_cloud_storage_default'
+)
+
+t1
