81b2fb06fc12b26bdfb63013e7fed01732f2b235
Joey Dreijer <jdreijer@schubergphilis.com>
2020-02-14 15:13:59 +0100

    test

---
+++ dags/sparkdag.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import airflow
+import datetime
+from datetime import timedelta
+from airflow.models import DAG
+from airflow.contrib.operators.postgres_to_gcs_operator import PostgresToGoogleCloudStorageOperator
+
+
+args = {
+    'owner': 'JDreijer',
+    'start_date': airflow.utils.dates.days_ago(2),
+}
+
+dag = DAG(
+    dag_id='sparkdag',
+    default_args=args,
+    schedule_interval= None,
+    dagrun_timeout=timedelta(minutes=60),
+)
+
+
+t1 = PostgresToGoogleCloudStorageOperator(
+    task_id='from_pg_to_gcs',
+    postgres_conn_id='postgres_default',
+    sql='select * from land_registry_price_paid_uk',
+    bucket='spark_bucket_jd',
+    filename='postgres_dump.csv',
+    google_cloud_storage_conn_id='google_cloud_storage_default',
+    dag=dag
+)
+
+t1
